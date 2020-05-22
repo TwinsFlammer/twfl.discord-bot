8afe823de1e8273deaeab663feebbc222935ca6f
Gutyerrez <srgutyerrez@gmail.com>
2020-05-22 06:55:56 -0300

    Create StartManager.java

---
+++ src/main/java/br/com/twinflammer/dicordbot/manager/StartManager.java
@@ -0,0 +1,12 @@
+package br.com.twinflammer.dicordbot.manager;
+
+import br.com.twinflammer.dicordbot.configuration.Configuration;
+
+/**
+ * @author SrGutyerrez
+ */
+public class StartManager {
+    public StartManager() {
+        new Configuration();
+    }
+}
