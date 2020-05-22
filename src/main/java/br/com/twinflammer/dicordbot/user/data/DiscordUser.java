2eb02b2d7d8c43c8aca89b59c5b0c7f3bafddd66
Gutyerrez <srgutyerrez@gmail.com>
2020-05-22 06:53:06 -0300

    Create DiscordUser.java

---
+++ src/main/java/br/com/twinflammer/dicordbot/user/data/DiscordUser.java
@@ -0,0 +1,70 @@
+package br.com.twinflammer.dicordbot.user.data;
+
+import br.com.twinflammer.dicordbot.DiscordBot;
+import br.com.twinsflammer.common.shared.permissions.group.data.Group;
+import br.com.twinsflammer.common.shared.permissions.user.data.User;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+
+import java.util.List;
+
+/**
+ * @author SrGutyerrez
+ */
+public class DiscordUser extends User {
+    public DiscordUser(User user) {
+        super(
+                user.getId(),
+                user.getName(),
+                user.getDisplayName(),
+                user.getUniqueId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getCash(),
+                user.getDiscordId(),
+                user.isTwoFactorAuthenticationEnabled(),
+                user.getTwoFactorAuthenticationCode(),
+                user.getCreatedAt(),
+                user.getFirstLogin(),
+                user.getLastLogin(),
+                user.getLastAddress(),
+                user.getLastLobbyId(),
+                user.getLanguageId(),
+                user.getTwitterAccessToken(),
+                user.getTwitterTokenSecret(),
+                user.getGroups(),
+                user.getPreferences(),
+                user.getFriends(),
+                user.getIgnored(),
+                user.getReports(),
+                user.getSkins(),
+                user.isChangingSkin(),
+                user.isWaitingTabListRefresh()
+        );
+    }
+
+    public void setRole(Guild guild, Role role) {
+        if (this.getDiscordId() == null) return;
+
+        Member member = this.getMember(guild);
+
+        member.getRoles().forEach(role1 -> guild.removeRoleFromMember(member, role1));
+
+        guild.addRoleToMember(member, role);
+    }
+
+    public void updateDisplayName(Guild guild) {
+        Member member = this.getMember(guild);
+
+        JDA jda = DiscordBot.getInstance().getClient();
+
+        member.modifyNickname(this.getDisplayName());
+    }
+
+    public Member getMember(Guild guild) {
+        return guild.getMemberById(this.getDiscordId());
+    }
+}
