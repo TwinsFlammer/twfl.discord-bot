2268b7750742878ac8ec3d0308926e305752589a
Gutyerrez <srgutyerrez@gmail.com>
2020-05-22 07:49:29 -0300

    Update QueueManager.java

--- src/main/java/br/com/twinsflammer/discordbot/queue/manager/QueueManager.java
+++ src/main/java/br/com/twinsflammer/discordbot/queue/manager/QueueManager.java
@@ -1,5 +1,7 @@
 package br.com.twinsflammer.discordbot.queue.manager;
 
+import br.com.twinsflammer.common.shared.permissions.user.data.User;
+import br.com.twinsflammer.common.shared.permissions.user.manager.UserManager;
 import br.com.twinsflammer.discordbot.user.data.DiscordUser;
 import br.com.twinsflammer.common.shared.Common;
 import com.google.common.collect.Queues;
@@ -14,6 +16,12 @@ public class QueueManager {
     private final Queue<DiscordUser> waitingUpdate = Queues.newConcurrentLinkedQueue();
 
     public QueueManager() {
+        User user = UserManager.getUser(2);
+
+        DiscordUser discordUser1 = new DiscordUser(user);
+
+        this.waitingUpdate.add(discordUser1);
+
         Common.getInstance().getScheduler().scheduleAtFixedRate(
                 () -> {
                     DiscordUser discordUser = waitingUpdate.poll();
