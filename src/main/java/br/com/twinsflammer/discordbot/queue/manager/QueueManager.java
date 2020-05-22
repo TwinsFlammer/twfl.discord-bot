d97b563b5d10b5aeb191a79cc0a12f1bea11b27a
Gutyerrez <srgutyerrez@gmail.com>
2020-05-22 07:56:58 -0300

    Update QueueManager.java

--- src/main/java/br/com/twinsflammer/discordbot/queue/manager/QueueManager.java
+++ src/main/java/br/com/twinsflammer/discordbot/queue/manager/QueueManager.java
@@ -1,10 +1,14 @@
 package br.com.twinsflammer.discordbot.queue.manager;
 
+import br.com.twinsflammer.common.shared.permissions.group.data.Group;
 import br.com.twinsflammer.common.shared.permissions.user.data.User;
 import br.com.twinsflammer.common.shared.permissions.user.manager.UserManager;
+import br.com.twinsflammer.discordbot.DiscordBot;
+import br.com.twinsflammer.discordbot.group.data.DiscordGroup;
 import br.com.twinsflammer.discordbot.user.data.DiscordUser;
 import br.com.twinsflammer.common.shared.Common;
 import com.google.common.collect.Queues;
+import net.dv8tion.jda.api.entities.Role;
 
 import java.util.Queue;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +30,14 @@ public class QueueManager {
                 () -> {
                     DiscordUser discordUser = waitingUpdate.poll();
 
+                    Group group = discordUser.getHighestGroup();
+                    DiscordGroup discordGroup = new DiscordGroup(group);
 
+                    DiscordBot.getInstance().getClient().getGuilds().forEach(guild -> {
+                        Role role = discordGroup.getRole(guild);
+
+                        discordUser.updateDisplayName(guild);
+                    });
                 },
                 0,
                 1,
