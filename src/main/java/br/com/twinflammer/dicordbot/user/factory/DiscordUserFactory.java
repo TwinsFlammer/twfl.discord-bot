134888c335d9c3f1ed9691004c3036e5610dadf6
Gutyerrez <srgutyerrez@gmail.com>
2020-05-22 06:53:40 -0300

    Create DiscordUserFactory.java

---
+++ src/main/java/br/com/twinflammer/dicordbot/user/factory/DiscordUserFactory.java
@@ -0,0 +1,35 @@
+package br.com.twinflammer.dicordbot.user.factory;
+
+import br.com.twinflammer.dicordbot.user.data.DiscordUser;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author SrGutyerrez
+ */
+public class DiscordUserFactory<U extends DiscordUser> {
+    private List<U> users = Lists.newArrayList();
+
+    public U getUser(Integer id) {
+        return this.users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public U getUser(UUID uniqueId) {
+        return this.users.stream()
+                .filter(u -> u.getUniqueId().equals(uniqueId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public U getUser(String name) {
+        return this.users.stream()
+                .filter(u -> u.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+}
